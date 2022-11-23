d69116f49f24454daeb5abd923aa347e915d8522
Moises <mhdezcoronado@gmail.com>
2022-11-23 11:15:33 +0100

    Setup join room

--- server/index.js
+++ server/index.js
@@ -21,11 +21,12 @@ io.on('connection', (socket) => {
   console.log(`connected clien with ID: ${socket.id}`);
 
   socket.on('join_room', (data) => {
+    socket.join(data)
     console.log(`user with ID: ${socket.id} joined room: ${data}`);
   })
 
   socket.on('send_message', (data) => {
-    console.log(data);
+    socket.to(data.room).emit('receive_message', data);
   })
 
   socket.on('disconnect', () => {
