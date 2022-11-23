30def394af7e465a0d8bc815592331fb05568acf
Moises <mhdezcoronado@gmail.com>
2022-11-23 10:40:44 +0100

    Create send message method

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,4 +1,21 @@
-export default function Chat({username, room}) {
+import { useState } from "react";
+
+export default function Chat({username, room, socket}) {
+  const [message, setMessage] = useState('');
+
+  const sendMessage = () => {
+    if (message !== '')  {
+      const data = {
+        username,
+        room,
+        message,
+      }
+
+      socket.emit('send_message', data);
+      setMessage('')
+    }
+  }
+
   return (
     <div className='live-chat'>
       <div className="chat-header">
@@ -8,8 +25,17 @@ export default function Chat({username, room}) {
       <div className="chat-body"></div>
 
       <div className="chat-footer">
-        <input className='chat-input' type="text" />
-        <button className='chat-send'>&#9658;</button>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className='chat-input'
+          type="text"
+        />
+
+        <button
+          className='chat-send'
+          onClick={sendMessage}
+        >&#9658;</button>
       </div>
     </div>
   );
