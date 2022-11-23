7a559cd69af47c9ccc6835961354cb8d2cb88c00
Moises <mhdezcoronado@gmail.com>
2022-11-23 10:08:34 +0100

    Set up join room

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,47 @@
+import { useState } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
 const socket = io.connect('http://localhost:3001');
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [room, setSRoom] = useState('');
 
+  const joinRoom = () => {
+    if (username !== '' && room !== '') {
+      socket.emit('join_room', room)
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="join-room-wrapper">
+        <h1>LIVE CHAT</h1>
+        <input
+          value={username}
+          className='join-input'
+          type="text"
+          placeholder='username...'
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          value={room}
+          className='join-input'
+          type="text"
+          placeholder='room...'
+          onChange={(e) => setSRoom(e.target.value)}
+        />
+
+        <button
+          className='join-button'
+          onClick={joinRoom}
+        >
+          Join room</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
